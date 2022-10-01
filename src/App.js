16a3dcd023288467bf8ea7e4d2d5ea80510ce974
Alan Antony James <s10207327@connect.np.edu.sg>
2022-10-01 21:36:53 +0800

    add function to add contact card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import React, {useState} from "react"
 import './App.css';
+import Header from './components/Header'
+import AddContact from './components/AddContact'
+import ContactList from './components/ContactList'
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, contact])
+  }
   return (
-    <div>Hello World!</div>
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler}/>
+      <ContactList contacts={contacts}/>
+    </div>
   );
 }
 
