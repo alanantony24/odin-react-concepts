4a6f57705a94b0420aa7fd130e5ee7008379579d
Alan Antony James <s10207327@connect.np.edu.sg>
2022-10-02 18:39:59 +0800

    finished app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,88 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { v4 as uuid } from "uuid";
 import './App.css';
+import api from './api/contacts';
 import Header from './components/Header'
 import AddContact from './components/AddContact'
 import ContactList from './components/ContactList'
 import ContactDetails from './components/ContactDetail'
+import EditContact from './components/EditContact'
 
 function App() {
-  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
-  const addContactHandler = (contact) => {
-    console.log(contact);
-    setContacts([...contacts, { id: uuid(), ...contact }])
+  //Retrieve contacts API
+  const retrieveContacts = async () => {
+    const response = await api.get("/contacts")
+    return response;
   }
 
-  const removeContactHandler = (id) => {
+  const addContactHandler = async (contact) => {
+
+    const request = {
+      id: uuid(),
+      ...contact
+    }
+
+    const response = await api.post("/contacts", request)
+    setContacts([...contacts, response.data])
+
+  }
+
+  const updateContactHandler = async (contact) => {
+    
+    const response = await api.put(`/contacts/${contact.id}`, contact);
+    const { id, name, email } = response.data;
+
+    setContacts(
+      contacts.map((contact) => {
+        return contact.id === id ? { ...response.data } : contact;
+      })
+    );
+    
+  };
+
+  const removeContactHandler = async (id) => {
+    api.delete(`/contacts/${id}`);
     const newContactList = contacts.filter((contact) => {
       return contact.id !== id;
     });
     setContacts(newContactList);
   }
 
+  const searchHandler = (searchTerm) => {
+    setSearchTerm(searchTerm);
+    if(searchTerm !== "") {
+      const newContactList = contacts.filter((contact) => {
+        return Object.values(contact)
+        .join(" ")
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase());
+      });
+      setSearchResults(newContactList);
+    }
+    else {
+      setSearchResults(contacts);
+    }
+  }
+  
   useEffect(() => {
-    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts)));
-    if (retrieveContacts) setContacts(retrieveContacts);
+
+    const getAllContacts = async () => {
+      const allContacts = await retrieveContacts();
+      if (allContacts) setContacts(allContacts);
+    };
+
+    getAllContacts();
+
   }, []);
 
   useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
   }, [contacts]);
 
   return (
@@ -44,8 +96,10 @@ function App() {
             render={(props) => (
               <ContactList
                 {...props}
-                contacts={contacts}
+                contacts={searchTerm.length < 1 ? contacts : searchResults}
                 getContactId={removeContactHandler}
+                term={searchTerm}
+                searchKeyWord={searchHandler}
               />
             )}
           />
@@ -54,6 +108,14 @@ function App() {
               <AddContact
                 {...props}
                 addContactHandler={addContactHandler}
+              />
+            )}
+          />
+          <Route path="/edit"
+            render={(props) => (
+              <EditContact
+                {...props}
+                updateContactHandler={updateContactHandler}
               />
             )}
           />
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useRef} from "react";
 import { Link } from "react-router-dom";
 import ContactCard from "./ContactCard";
 
 const ContactList = (props) => {
+
     console.log(props);
+    const inputEl = useRef("");
 
     const deleteContactHandler = (id) => {
         props.getContactId(id);
@@ -15,13 +17,23 @@ const ContactList = (props) => {
         );
     });
 
+    const getSearchTerm = () => {
+        props.searchKeyword(inputEl.current.value)
+    }
+
     return (
         <div className="main">
             <h2>Contact List
                 <Link to="/add"> <button className="ui button blue right">Add Contact</button></Link>
             </h2>
+            <div className="ui search">
+                <div className="ui icon input">
+                    <input ref={inputEl} type="text" placeholder="Search Contacts" className="prompt" value={props.term} onChange={getSearchTerm} />
+                    <i className="search icon"></i>
+                </div>
+            </div>
             <div className="ui celled list">
-                {renderContactsList}
+                {renderContactsList.length > 0 ? renderContactsList : "No contacts available"}
             </div>
         </div>
     )
